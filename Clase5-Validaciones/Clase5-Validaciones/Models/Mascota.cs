9ae9d96723d6313d01164730308685d495edbedd
Pablo Sanchez <psanchez@plus54.com>
2021-05-05 22:08:44 -0300

    agregando validacion al combo de Especies y al rango de peso

--- Clase5-Validaciones/Clase5-Validaciones/Models/Mascota.cs
+++ Clase5-Validaciones/Clase5-Validaciones/Models/Mascota.cs
@@ -9,12 +9,12 @@ namespace Clase5_Validaciones.Models
     public class Mascota
     {
         public int Id { get; set; }
-        //[Required(ErrorMessage ="Campo requerido")]
+        [Required(ErrorMessage ="Campo requerido")]
         public Especie Especie { get; set; }
         [Required(ErrorMessage ="Campo requerido")]
         public string Estado { get; set; }
-        //[Range(0, 5000, ErrorMessage ="El peso debe estar entre 0 y 5000")]
-        public int Peso { get; set; }
+        [Range(0, 5000, ErrorMessage = "El peso debe estar entre 0 y 5000")]
+        public int? Peso { get; set; }
         public string Color { get; set; }
         [StringLength(200, ErrorMessage ="No se puede ingresar mas de 200 caracteres")]
         public string Nombre { get; set; }
