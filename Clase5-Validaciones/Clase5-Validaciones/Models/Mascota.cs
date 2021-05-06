e3e4d5d6c08cc2a54137dd9b6bbed51cff9c4888
Pablo Sanchez <psanchez@plus54.com>
2021-05-05 22:01:41 -0300

    agregando validacion de requerido y longitud

--- Clase5-Validaciones/Clase5-Validaciones/Models/Mascota.cs
+++ Clase5-Validaciones/Clase5-Validaciones/Models/Mascota.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,14 @@ namespace Clase5_Validaciones.Models
     public class Mascota
     {
         public int Id { get; set; }
+        //[Required(ErrorMessage ="Campo requerido")]
         public Especie Especie { get; set; }
+        [Required(ErrorMessage ="Campo requerido")]
         public string Estado { get; set; }
-        public float Peso { get; set; }
+        //[Range(0, 5000, ErrorMessage ="El peso debe estar entre 0 y 5000")]
+        public int Peso { get; set; }
         public string Color { get; set; }
+        [StringLength(200, ErrorMessage ="No se puede ingresar mas de 200 caracteres")]
         public string Nombre { get; set; }
         public string DatosDeContacto { get; set; }
         public List<string> Fotos { get; set; }
